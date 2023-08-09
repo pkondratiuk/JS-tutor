645abbfc1b7d80e6bd5053548a637e89e8a4f6c6
Pavlo <95445682+pkondratiuk@users.noreply.github.com>
2023-08-09 21:38:57 +0300

    JS UPD
    add some HW

---
+++ js/index.js
@@ -0,0 +1,27 @@
+// const productName = "Droid";
+// const pricePerItem = 2000;
+
+// console.log(productName);
+// console.log(pricePerItem);
+
+// let productName = "Repair droid";
+// let pricePerItem = 1500;
+
+
+// const topSpeed = 160;
+// const distance = 614.54;
+// const login = "mango935";
+// const isOnline = true;
+// const isAdmin = false;
+
+
+// const massage = `You picked ${productName}, price per item is ${pricePerItem} `
+
+// Task 6 
+const pricePerDroid = 800;
+const orderedQuantity = 6;
+const deliveryFree = 50;
+const totalPrice = (pricePerDroid*orderedQuantity) + deliveryFree;
+const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFree} credits) is included in total price.`
+
+console.log(message);
