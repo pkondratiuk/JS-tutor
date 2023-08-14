797b17cc49af37091573b72ed685b0cb52f6b6a0
Pavlo <95445682+pkondratiuk@users.noreply.github.com>
2023-08-15 00:22:14 +0300

    UPD
    finish all autocheck

--- js/index.js
+++ js/index.js
@@ -18,10 +18,95 @@
 // const massage = `You picked ${productName}, price per item is ${pricePerItem} `
 
 // Task 6 
-const pricePerDroid = 800;
-const orderedQuantity = 6;
-const deliveryFree = 50;
-const totalPrice = (pricePerDroid*orderedQuantity) + deliveryFree;
-const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFree} credits) is included in total price.`
 
-console.log(message);
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFree = 50;
+// const totalPrice = (pricePerDroid*orderedQuantity) + deliveryFree;
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFree} credits) is included in total price.`
+
+// console.log(message);
+
+// Task 7 
+
+// function sayHi {
+//     console.log("Hello, this is my first function!");
+// }
+
+// sayHi();
+
+// Task 8
+
+// function add(a, b, c){
+// console.log(`Addition result equals ${a+b+c}`);
+// return a+b+c;
+// }
+// add(15, 27, 10);
+
+//  Task 10
+
+// function makeMessage (name, price){
+//     console.log(`You picked ${name}, price per item is ${price} credits`);
+//     const message = ;
+//     return message;
+// }
+
+// if (ordered ====0){
+//     message = "There are no products in the order!"
+// }
+
+// else if(ordered>available){
+//     message = "Your order is too large, there are not enough items in stock!"
+// }
+
+// else {
+//     message = 'The order is accepted, our manager will contact you"
+// }
+
+// const isInRange = number>=start && number<=encodeURI;
+
+// if (totalSpent >= 50000){
+//     discount = GOLD_DISCOUNT;
+// };
+//  else if(totalSpent>20000 && totalSpent<50000){
+//     discount = SILVER_DISCOUNT;
+//  }
+
+//  else if (totalSpent>5000 && totalSpent<20000){
+//     discount = BRONZE_DISCOUNT;
+//  }
+
+//  else if (totalSpent>0 && totalSpent<5000){
+//     discount = BASE_DISCOUNT;
+//  }
+
+//  else
+
+
+
+switch (country) {
+    case "China":
+        message = "Shipping to China will cost 100 credits"
+        
+        break;
+
+        case "Chile":
+        message = "Shipping to Chile will cost 250 credits"
+        
+        break;
+
+        case "Australia":
+        message = "Shipping to Australia will cost 170 credits"
+        
+        break;
+
+        case "Jamaica":
+            message = "Shipping to Jamaica will cost 120 credits"
+            
+            break;
+
+    default:
+        message= "Sorry, there is no delivery to your country"
+        
+}
+toLove
